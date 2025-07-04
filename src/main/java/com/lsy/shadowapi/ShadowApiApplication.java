feb66dc323a10720da5f684b1c9745133de9f08f
이상엽 <infightersy@naver.com>
2025-07-04 23:16:39 +0900

    init: first commit

---
+++ src/main/java/com/lsy/shadowapi/ShadowApiApplication.java
@@ -0,0 +1,13 @@
+package com.lsy.shadowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShadowApiApplication.class, args);
+    }
+
+}
