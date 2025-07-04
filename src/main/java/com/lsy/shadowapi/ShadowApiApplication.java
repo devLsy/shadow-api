1e699cb17cc02ca03d1e10e78a8b603c33800dcd
이상엽 <infightersy@naver.com>
2025-07-05 08:15:38 +0900

    add: application.yml 정보 암복호화 모듈 적용

--- src/main/java/com/lsy/shadowapi/ShadowApiApplication.java
+++ src/main/java/com/lsy/shadowapi/ShadowApiApplication.java
@@ -1,8 +1,10 @@
 package com.lsy.shadowapi;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEncryptableProperties    // springBoot 3.2.x버전에서 애노테이션 적용해야 복호화 가능
 @SpringBootApplication
 public class ShadowApiApplication {
 
